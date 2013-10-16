a02dcf54bcf60fe3a6299d64fb5e4a103bc38c39
Carl Danley <carldanley@gmail.com>
2013-10-16 18:27:08 -0400

    added the plugin file for WP

---
+++ scrivener.php
@@ -0,0 +1,9 @@
+/**
+Plugin Name: Scrivener
+Plugin URI: http://10up.com
+Description: A WordPress plugin to enhance the post editor preview, mainly through proof of concept.
+Version: 0.1.0
+Author: 10up
+Author URI: http://10up.com
+License: GPLv2 or later
+*/

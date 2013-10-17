f6208788bfd3d72f5320bd15625fb3de14ffa621
Carl Danley <carldanley@gmail.com>
2013-10-17 01:06:15 -0400

    removed old references to files that no longer exist.

--- scrivener.php
+++ scrivener.php
@@ -8,11 +8,3 @@ Author: 10up
 Author URI: http://10up.com
 License: GPLv2 or later
  */
-
-if ( ! class_exists( '\\Composer\\Autoload\\ClassLoader' ) ) {
-	// include our default customizer classes
-	include_once( __DIR__ . '/classes/Customizer/Control.php' );
-	include_once( __DIR__ . '/classes/Customizer/Section.php' );
-	include_once( __DIR__ . '/classes/Customizer/Core.php' );
-	include_once( __DIR__ . '/classes/Scrivener/Core.php' );
-}

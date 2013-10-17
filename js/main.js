4b1c127dde39c62d9b260003e6bd58bb9657b8b6
Carl Danley <carldanley@gmail.com>
2013-10-17 02:18:04 -0400

    redid options passing

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
-( function( Scrivener, window, $ ) {
+( function( Scrivener, window, $, Scrivener_Data ) {
 
 	"use strict";
 	var document = window.document;
 
 	$( document ).ready( function() {
 
-		var coreModel = new Scrivener.Models.Core();
+		var coreModel = new Scrivener.Models.Core( {
+			localizedData : Scrivener_Data
+		} );
 		var coreView = new Scrivener.Views.Core( {
 			model : coreModel
 		} );
 
 	} );
 
-} )( Scrivener, window, jQuery );
+} )( Scrivener, window, jQuery, Scrivener_Data );

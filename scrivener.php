25af96ec0aa9018bfea77a3b0f653168831eb9ae
John P. Bloch <john.bloch@10up.com>
2013-10-16 20:53:24 -0400

    Include Control.php for non-autoloaded installs

--- scrivener.php
+++ scrivener.php
@@ -11,6 +11,7 @@ License: GPLv2 or later
 
 if ( ! class_exists( '\\Composer\\Autoload\\ClassLoader' ) ) {
 	// include our default customizer classes
+	include_once( __DIR__ . '/classes/Customizer/Control.php' );
 	include_once( __DIR__ . '/classes/Customizer/Section.php' );
 	include_once( __DIR__ . '/classes/Customizer/Core.php' );
 	include_once( __DIR__ . '/classes/Scrivener/Core.php' );

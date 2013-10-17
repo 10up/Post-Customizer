402cee6ecee48371b0ae3a114e7a014ff500c627
Taylor Lovett <admin@taylorlovett.com>
2013-10-16 22:19:27 -0700

    Add meat to preview link filter

--- classes/class-scrivener.php
+++ classes/class-scrivener.php
@@ -4,12 +4,28 @@ class Scrivener {
 
 	private static $_instance;
 
+	/**
+	 * Setup singleton Scrivener instance
+	 *
+	 * @since 0.1.0
+	 * @uses add_filter
+	 * @return void
+	 */
 	private function __construct() {
 		add_filter( 'preview_post_link', array( $this, 'filter_preview_post_link' ) );
 	}
 
-	public function filter_preview_post_link() {
-
+	/**
+	 * Filter preview post link
+	 *
+	 * @param string $link
+	 * @since 0.1.0
+	 * @uses admin_url
+	 * @return string
+	 */
+	public function filter_preview_post_link( $link ) {
+		global $post;
+		return admin_url( 'admin-post.php?weiverp=1&p=' . $post->ID );
 	}
 
 	/**
@@ -20,7 +36,6 @@ class Scrivener {
 	 */
 	public function init() {
 		if ( ! isset( self::$_instance ) ) {
-
 			self::$_instance = new Scrivener;
 		}
 
@@ -28,3 +43,5 @@ class Scrivener {
 	}
 
 }
+
+Scrivener::init();

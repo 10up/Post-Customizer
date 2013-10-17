d3a1733d6e694af44f535ac014f578b8539f30c3
John P. Bloch <john.bloch@10up.com>
2013-10-16 20:43:50 -0400

    Add a get_control and doc blocks for methods

--- classes/Customizer/Section.php
+++ classes/Customizer/Section.php
@@ -4,11 +4,36 @@ namespace Customizer;
 
 class Section {
 
+	public $id;
+
+	protected $controls = array();
+
+	/**
+	 * Add a control to the section
+	 *
+	 * @param Control $control
+	 */
 	public function add_control( Control $control ) {
 
 	}
 
-	public function remove_control( Control $control ) {
+	/**
+	 * Get a control object by ID
+	 *
+	 * @param string $control_id
+	 *
+	 * @return Control
+	 */
+	public function get_control( $control_id ) {
+
+	}
+
+	/**
+	 * Remove a control by ID
+	 *
+	 * @param string $control_id
+	 */
+	public function remove_control( $control_id ) {
 
 	}
 

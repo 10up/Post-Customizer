3ba0537f49afde43a10713cc3d81f180b84529b5
Taylor Lovett <admin@taylorlovett.com>
2013-10-16 22:10:33 -0700

    Add basic Scrivener class structure and preview_post_link filter skeleton

--- classes/class-scrivener.php
+++ classes/class-scrivener.php
@@ -1,1 +1,30 @@
 <?php
+
+class Scrivener {
+
+	private static $_instance;
+
+	private function __construct() {
+		add_filter( 'preview_post_link', array( $this, 'filter_preview_post_link' ) );
+	}
+
+	public function filter_preview_post_link() {
+
+	}
+
+	/**
+	 * Initialize class and return an instance of it
+	 *
+	 * @since 0.1.0
+	 * @return Scrivener
+	 */
+	public function init() {
+		if ( ! isset( self::$_instance ) ) {
+
+			self::$_instance = new Scrivener;
+		}
+
+		return self::$_instance;
+	}
+
+}
